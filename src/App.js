79b80e57764903fb29a5755bd42596e47841ed57
Aline <aline.santoro@dianurse.com>
2020-09-22 11:36:48 -0300

    styled navBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import VideoChatComponent from "./VideoChatComponent";
-import logo from "./assets/vonagelogo.png";
+import logo from "./assets/dianurse-logo.png";
+import logoIcon from "./assets/logo-icon.png";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <div className="nav-bar">
-        <p>Vonage Video Chat Application</p>
-        <img src={logo} alt='vonage logo' className='logo'/>
+        <div className="title">
+          <img src={logoIcon} alt="logo icon" className="logoIcon" />
+          <p>Dianurse Video Chat Application</p>
+        </div>
+        <img src={logo} alt="dianurse logo" className="logo" />
       </div>
       <VideoChatComponent />
     </div>
